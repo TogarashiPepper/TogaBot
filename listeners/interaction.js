211a12618da69a0be329059c21ab13872af6768c
Togarashi <Toga@Togarashi>
2021-06-05 22:54:14 +0000

    added functionality so only message author can delete buttons

--- listeners/interaction.js
+++ listeners/interaction.js
@@ -14,7 +14,15 @@ class interactionListener extends Listener
 	{
 		if(interaction.isMessageComponent() && interaction.componentType == 'BUTTON')
 		{
-			
+			const authorID = interaction.customID.split('-');
+			if(interaction.customID.startsWith('delete') && authorID[1] === interaction.user.id)
+			{
+				interaction.message.delete();
+			}
+			else
+			{
+				interaction.deferUpdate()
+			}
 		}
 	}
 }
