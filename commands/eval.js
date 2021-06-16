b78245359b6b18d6c36630b7fd20906aa4042524
Togarashi <Toga@Togarashi>
2021-06-16 01:06:27 +0000

    added file for evaled over 2k chars

--- commands/eval.js
+++ commands/eval.js
@@ -1,5 +1,5 @@
 const { Command } = require('discord-akairo');
-const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
+const { MessageEmbed, MessageActionRow, MessageButton, MessageAttachment} = require('discord.js');
 const delButton = new MessageButton().setCustomID('delete-779403924850343947').setLabel('delete').setStyle('SECONDARY');
 
 const row = new MessageActionRow().addComponents(delButton);
@@ -33,11 +33,16 @@ class evalCommand extends Command {
 			// if(message.content.match(flagregex)) {
 			// message.flags.push(message.content.match(flagregex)[0]);
 			let evaled = await eval('(async () => {' + result + '})()');
-			if (evaled !== null || evaled !== undefined) {
-				if (typeof evaled != 'string') evaled = require('util').inspect(evaled, { depth: 0 });
+			if(`\`\`\`js\n${evaled}\`\`\``.length < 2000) {
+				if (typeof evaled !== 'string') {
+					if (typeof evaled != 'string') evaled = require('util').inspect(evaled, { depth: 0 });
+				}
 				message.channel.send({ content: `\`\`\`js\n${evaled}\`\`\``, components: [row] });
 			}
-
+			else{
+				const file = new MessageAttachment(Buffer.from(`${evaled}`), 'eval.js');
+				message.channel.send({ content: 'the result of eval was over 2000 characters so it has been converted to a file', files: [file], components: [row] })
+			}
 			// }
 		}
 		catch (err) {
