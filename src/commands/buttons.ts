cd2a1f6516f8d3028aae12a6e4e589db6ce544e9
Brage <37003366+hellobafu@users.noreply.github.com>
2021-06-26 09:09:42 +0200

    feat: using util/buttons

--- src/commands/buttons.ts
+++ src/commands/buttons.ts
@@ -1,6 +1,6 @@
 import { Command } from 'discord-akairo';
 import { MessageButton, MessageActionRow, Message } from 'discord.js';
-import { oneButton, deleteButton } from '../util/buttons.js';
+import { createOneButton, createDeleteButton } from '../util/buttons.js';
 
 export default class ButtonCommand extends Command {
 	constructor() {
@@ -10,8 +10,8 @@ export default class ButtonCommand extends Command {
 	}
 
 	exec(message: Message) {
-		const oneButton = oneButton(message);
-		const delButton = deleteButton(message);
+		const oneButton = createOneButton(message);
+		const delButton = createDelButton(message);
 
 		const row = new MessageActionRow().addComponents(oneButton, delButton);
 		message.channel.send({ content: 'hello', components: [row] });
