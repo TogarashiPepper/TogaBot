601b6d5064f8469f8d3e1f5171940995bb5099a2
TogarashiPepper <83902606+TogarashiPepper@users.noreply.github.com>
2021-06-26 20:52:23 -0700

    refactor: changed src/util/buttons.ts to export a single func

--- src/util/buttons.ts
+++ src/util/buttons.ts
@@ -1,24 +1,29 @@
 import { Message, MessageButton } from "discord.js";
 
-const deleteButton = (message: Message) => {
-	return new MessageButton()
-		.setCustomID(`delete-${message.author.id}`)
-		.setLabel('delete')
-		.setStyle('SECONDARY');
-}
-
-const oneButton = (message: Message) => {
-	return new MessageButton()
+const makeButtton = (message: Message, style: string) => {
+	if(style === '1'){
+		return new MessageButton()
 		.setCustomID(`1-${message.author.id}`)
 		.setLabel('1')
 		.setStyle('PRIMARY');
-}
-
-const twoButton = (message: Message) => {
-	return new MessageButton()
+	}
+	else if(style === '2'){
+		return new MessageButton()
 		.setCustomID(`2-${message.author.id}`)
 		.setLabel('2')
 		.setStyle('DANGER');
-}
-
-export { deleteButton, oneButton, twoButton };
+	}
+	else if(style === 'delete'){
+		return new MessageButton()
+		.setCustomID(`delete-${message.author.id}`)
+		.setLabel('delete')
+		.setStyle('SECONDARY');
+	}
+	else {
+		return new MessageButton()
+		.setCustomID(`delete-${message.author.id}`)
+		.setLabel('delete')
+		.setStyle('SECONDARY');
+	}
+};
+export default makeButtton;
