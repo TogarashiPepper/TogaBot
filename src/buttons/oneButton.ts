739391a2f529622b70b38e393625f5eaffd231f8
TogarashiPepper <83902606+TogarashiPepper@users.noreply.github.com>
2021-06-28 21:32:00 -0700

    chore: moved buttons to buttonFiles

---
+++ src/buttons/oneButton.ts
@@ -0,0 +1,14 @@
+import { ButtonInteraction, MessageActionRow } from "discord.js";
+import createButton from '../util/buttons'
+
+const button = { customID: '1', execute: (interaction: ButtonInteraction) => {
+	const row = new MessageActionRow()
+	.addComponents([
+		createButton(interaction.user.id, '2'),
+		createButton(interaction.user.id, 'delete')
+	]);
+
+	interaction.update({ content: 'hello', components: [row] });
+}}
+
+export = button;
