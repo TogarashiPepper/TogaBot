f9f5abf9acb3f3f94eede22502e4750aec6f4a20
TogarashiPepper <83902606+TogarashiPepper@users.noreply.github.com>
2021-06-30 18:06:11 -0700

    feat: added select menus

--- src/commands/General/activity.ts
+++ src/commands/General/activity.ts
@@ -1,4 +1,4 @@
-import { Message } from "discord.js";
+import { Message, MessageActionRow, MessageComponentInteraction, MessageSelectMenu, Snowflake } from "discord.js";
 import { Command, PieceContext } from '@sapphire/framework';
 import { APIInvite } from 'discord-api-types/v8';
 import activity from "../../util/activityFetch";
@@ -17,10 +17,24 @@ export default class extends Command {
 			const member = message.guild.me;
 			const channel = message.guild.channels.cache.find(c => c.type === 'voice' && c.permissionsFor(member).has('CREATE_INSTANT_INVITE'));
 			
-			if(channel) {
-			activity(message, '755600276941176913')
-			.then((res) => res.json()).then((i: APIInvite) => message.channel.send({ content: `https://discord.gg/${i.code}` }));
-			}
+			const select = new MessageSelectMenu().setCustomID('select').setPlaceholder('select something!')
+			.addOptions([
+			{ label:'Youtube Together', value: '755600276941176913' },
+			{ label: 'Fishington', value: '814288819477020702' },
+			{ label: 'betrayal', value: '773336526917861400' }
+			])
+			const row = new MessageActionRow().addComponents([select]);
+
+			const reply = await message.reply({ content: 'select an activity start', components: [row] })
+			const filter = (i: MessageComponentInteraction) => { return i.isSelectMenu() };
+			const collector = message.channel.createMessageComponentInteractionCollector({ filter: filter, time: 15000 });
+
+			collector.on('collect', (interaction: MessageComponentInteraction) => {
+				if(interaction.isSelectMenu()){
+					activity(message, interaction.values?.join('') as Snowflake)
+					.then((res) => res.json()).then((i: APIInvite) => interaction.update({ content: `https://discord.gg/${i.code}`, components: [] }));
+				}	
+			});
 		}
 	}
 };
