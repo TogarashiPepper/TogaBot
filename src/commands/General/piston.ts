eefdf4b1b179f45060ba830196ae23591cd1aa12
TogarashiPepper <83902606+TogarashiPepper@users.noreply.github.com>
2021-07-08 20:32:45 -0700

    feat: added multi lang eval using piston api

---
+++ src/commands/General/piston.ts
@@ -0,0 +1,54 @@
+import { Message, MessageAttachment, MessageEmbed } from "discord.js";
+import { Command, PieceContext } from '@sapphire/framework';
+import { inspect } from "util";
+import fetch from "node-fetch";
+const api = "https://emkc.org/api/v1/piston/execute";
+interface Fetched {
+	ran: boolean;
+	language: string;
+	version: string;
+	output: string;
+	stdout: string;
+	stderr: string;
+}
+
+export default class extends Command {
+	constructor(context: PieceContext) {
+		super(context, {
+			name: 'piston',
+			description: 'evaluate code in other langs'
+		});
+	}
+
+	async run(message: Message) {
+		const args = message.content.slice('?'.length).replace(/\n/g, ' ').split(/ +/)
+		args.shift();
+		const lang = args.shift()?.slice(3);
+		const code = message.content.match(/```[a-zA-Z+]*\n([\s\S]*?)```/);
+
+		if(code) {
+			const body = {
+				"language": lang,
+				"source": code[1],
+			}
+			const fetched: Fetched = await (await fetch(api, { method: 'post', body:JSON.stringify(body), headers: { 'Content-Type': 'application/json' }})).json();
+			const resultEmbed = new MessageEmbed();
+			if(fetched.stderr) {
+				resultEmbed.setTitle('oops, there was an error')
+				.setDescription(`\`\`\`${fetched.stderr}\`\`\``);
+				message.reply({ embeds: [resultEmbed] })
+			}
+			else if(fetched.stdout){
+				resultEmbed.setTitle(`successfully ran ${fetched.language}`)
+				.setDescription(`\`\`\`${lang}\n${fetched.stdout}\`\`\``);
+				message.reply({ embeds: [resultEmbed] })	
+			}
+			else{
+				message.reply("your code produced errors or output")
+			}
+
+		} else {
+			message.reply('you must provide code');
+		}
+	}
+};
