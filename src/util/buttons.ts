e7bd9c6c2f7698437541f099f7c57010f07ff1c0
TogarashiPepper <83902606+TogarashiPepper@users.noreply.github.com>
2021-07-08 20:32:17 -0700

    fix: fixed customID to be customId

--- src/util/buttons.ts
+++ src/util/buttons.ts
@@ -3,25 +3,25 @@ import { MessageButton, Snowflake } from "discord.js";
 const makeButtton = (id: Snowflake, style: string) => {
 	if(style === '1'){
 		return new MessageButton()
-		.setCustomID(`1-${id}`)
+		.setCustomId(`1-${id}`)
 		.setLabel('1')
 		.setStyle('PRIMARY');
 	}
 	else if(style === '2'){
 		return new MessageButton()
-		.setCustomID(`2-${id}`)
+		.setCustomId(`2-${id}`)
 		.setLabel('2')
 		.setStyle('DANGER');
 	}
 	else if(style === 'delete'){
 		return new MessageButton()
-		.setCustomID(`delete-${id}`)
+		.setCustomId(`delete-${id}`)
 		.setLabel('delete')
 		.setStyle('SECONDARY');
 	}
 	else {
 		return new MessageButton()
-		.setCustomID(`delete-${id}`)
+		.setCustomId(`delete-${id}`)
 		.setLabel('delete')
 		.setStyle('SECONDARY');
 	}
